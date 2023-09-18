bea6d8ba6c0a254703093ba1212811059861a286
Kemal1101 <lataxa.11@gmail.com>
2023-09-18 09:57:22 +0700

    Jobsheet 4 Percobaan 2 no.2

---
+++ Gaji23.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Gaji23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jmlMasuk, jmlTidakMasuk, TotGaji;
+        int gaji, potGaji;
+
+        System.out.println("Masukkan Jumlah Hari Masuk Kerja Anda: ");
+        jmlMasuk = sc.nextInt();
+        System.out.println("Masukkan Jumlah Hari Tidak Masuk Kerja Anda: ");
+        jmlTidakMasuk = sc.nextInt();
+
+        System.out.println("Masukkan Gaji Anda: ");
+        gaji = sc.nextInt();
+        System.out.println("Masukkan Potongan Gaji: ");
+        potGaji = sc.nextInt();
+        
+        TotGaji = (jmlMasuk * gaji) - (jmlTidakMasuk * potGaji);
+        System.out.println("Gaji Yang Anda Terima adalah: " + TotGaji);
+
+    }
+}
