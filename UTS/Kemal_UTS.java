c765936e0c744fbadcd62d5989ede719127c3b9c
Kemal1101 <lataxa.11@gmail.com>
2023-10-22 13:14:39 +0700

    menyempurnakan

--- UTS/Kemal_UTS.java
+++ UTS/Kemal_UTS.java
@@ -5,15 +5,21 @@ public class Kemal_UTS{
     
     Scanner sc = new Scanner (System.in);
 
-    int potensiAkademik, bahasaInggris, wawancara, jumlahMahasiswa, i = 1;
+    int potensiAkademik, bahasaInggris, wawancara, jumlahMahasiswa,siswaKe = 0, i = 1, jumlahLolosatauTdk = 0, ratarata = 0;
     float nilaiAkhir;
     String jurusan;
-
+    
     System.out.print("Masukkan Jumlah Calon Mahasiwa: ");
     jumlahMahasiswa = sc.nextInt();
-
+    
+    String namaMahasiswa [] = new String[jumlahMahasiswa];
+    String lolosatauTidak [] = new String[jumlahMahasiswa]; 
+    Float NilaiAkhir [] = new Float[jumlahMahasiswa];
+    
     while (i <= jumlahMahasiswa){
-    System.out.println("MASUKKAN NILAI MAHASISWA KE " + i);
+    System.out.print("MASUKKAN NAMA MAHASISWA: ");
+    namaMahasiswa [siswaKe] = sc.next();
+    siswaKe++;
     System.out.print("Masukkan Nilai Potensi Akademik (0-100): ");
     potensiAkademik = sc.nextInt();
     System.out.print("Masukkan Nilai Tes Bahasa Inggris (0-100): ");
@@ -21,38 +27,65 @@ public class Kemal_UTS{
     System.out.print("Masukkan Nilai Wawancara (0-100): ");
     wawancara = sc.nextInt();
     nilaiAkhir = (potensiAkademik * 0.5F) + (bahasaInggris * 0.3F) + (wawancara * 0.2F);
+     
+    NilaiAkhir [ratarata] = nilaiAkhir;
+    ratarata++;
 
     System.out.print("1. Sistem Informasi\n2. Ilmu Komputer\n3. Bisnis Manajemen\n4. Teknik Informatika\nMasukkan Angka dari Jurusan yang Anda Daftar: ");
     jurusan = sc.next();
     if (jurusan.equalsIgnoreCase("1")){
         if(bahasaInggris >= 70 && wawancara > 80){
-            System.out.println("Mahasiswa LOLOS");
+             lolosatauTidak [jumlahLolosatauTdk] = "MAHASISWA LOLOS";
+            jumlahLolosatauTdk++;
         }else{
-            System.out.println("Mahasiswa TIDAK LOLOS");
+             lolosatauTidak [jumlahLolosatauTdk] = "MAHASISWA TIDAK LOLOS";
+           jumlahLolosatauTdk++;
         }
     }else if (jurusan.equalsIgnoreCase("2")){
         if((potensiAkademik > 70 && bahasaInggris > 70 && wawancara > 70) || potensiAkademik == 100 || bahasaInggris == 100 || wawancara == 100){
-            System.out.println("Mahasiswa LOLOS");
+             lolosatauTidak [jumlahLolosatauTdk] = "MAHASISWA LOLOS";
+           jumlahLolosatauTdk++;
         }else {
-            System.out.println("Mahasiswa TIDAK LOLOS");
+             lolosatauTidak [jumlahLolosatauTdk] = "MAHASISWA TIDAK LOLOS";
+            jumlahLolosatauTdk++;
         }
     }else if (jurusan.equalsIgnoreCase("3")) {
         if (nilaiAkhir >= 75 && bahasaInggris >= 50){
-            System.out.println("Mahasiswa LOLOS");
+            lolosatauTidak [jumlahLolosatauTdk] = "MAHASISWA LOLOS";
+            jumlahLolosatauTdk++;
         }else{
-            System.out.println("Mahasiswa TIDAK LOLOS");
+            lolosatauTidak [jumlahLolosatauTdk] = "MAHASISWA TIDAK LOLOS";
+           jumlahLolosatauTdk++;
         }
     }else if (jurusan.equalsIgnoreCase("4")){
         if ((potensiAkademik >= 60 && bahasaInggris >= 60 && wawancara >= 60) || (potensiAkademik == 100 && bahasaInggris == 100) ){
-            System.out.println("Mahasiswa LOLOS");
+             lolosatauTidak [jumlahLolosatauTdk] = "MAHASISWA LOLOS";
+           jumlahLolosatauTdk++;
         }else{
-            System.out.println("Mahasiswa TIDAK LOLOS");
+             lolosatauTidak [jumlahLolosatauTdk] = "MAHASISWA TIDAK LOLOS";
+            jumlahLolosatauTdk++;
         }
     }else{
         System.out.println("Input eror");
     }
-
     i++;
+}
+System.out.println("PENGUMUMAN HASIL");
+System.out.println("---------------------");
+int j = 0, k = 1, l = 0, m = 0;
+while (j < jumlahMahasiswa){
+    System.out.println(k + "." + " " + namaMahasiswa[j]);
+    j++; 
+    k++;
+    while (l < j){
+        System.out.println(lolosatauTidak[l]);
+        l++;
+    }while(m < l){
+        System.out.println("Nilai Akhir Mahasiswa: " + NilaiAkhir [m]);
+        m++;
+    }
+   
+    
 }
 
 
