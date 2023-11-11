5801f357a0b6eaf9633f0267099748d07c3d3cde
Kemal1101 <lataxa.11@gmail.com>
2023-11-11 09:26:53 +0700

    update

--- Jobsheet 11/Individu1.java
+++ Jobsheet 11/Individu1.java
@@ -3,17 +3,22 @@ public class Individu1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
+        //untuk input nilai n
         System.out.print("Masukkan Nilai N: ");
         int n = sc.nextInt();
+        //untuk melakukan perulangan ketika n kurang dari 3
         while(n < 3){
             System.out.println("Nilai N minimal 3 silahkan coba lagi");
             System.out.print("Masukkan Nilai N: ");
             n = sc.nextInt();
         }
+        //perulangan untuk print per baris sampai nilai i kurang dari sama dengan n
         for (int i = 1 ; i <= n ; i++ ){
+            //perulangan untuk print jarak sampai nilai j lebih dari nilai n - nilai i
             for (int j = 1 ; j <= (n-i) ; j++){
                 System.out.print(" ");
             }
+            //perulangan untuk print nilai k dan nilai k akan terus bertambah hingga nilai k lebih dari i
             for (int k = 1 ; k <= i ; k++){
                 System.out.print(k);
             }
--- Jobsheet 11/Individu2.java
+++ Jobsheet 11/Individu2.java
@@ -3,15 +3,19 @@ public class Individu2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //untuk memasukkan nilai n
         System.out.print("Masukkan Nilai N: ");
         int n = sc.nextInt();
 
+        //untuk melakukan perulangan input nilai n jika nilai n kurang dari 5
         while (n < 5){
             System.out.println("Nilai N minimal 5 silahkan coba lagi");
             System.out.print("Masukkan Nilai N: ");
             n = sc.nextInt();
         }
 
+        //untuk melakukan perulangan print bintang sesuai input an dan nilai j akan berkurang terus 
+        //hingga nilai j sama dengan n
         for (int i = 0 ; i < n ; i++){
             for (int j = n ; j > i ; j--){
                 System.out.print("*");
