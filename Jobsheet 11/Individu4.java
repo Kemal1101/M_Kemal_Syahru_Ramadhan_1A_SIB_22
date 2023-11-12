1e49d63e155f7db6fcfc19fcc1ddb51561ad05f5
Kemal1101 <lataxa.11@gmail.com>
2023-11-12 15:42:56 +0700

    update

--- Jobsheet 11/Individu4.java
+++ Jobsheet 11/Individu4.java
@@ -6,14 +6,14 @@
             String inputCabor, namaAtlit, konfirmasi, konfirmasi2;
 
             do{
-                System.out.print("Cabang Olahraga yang akan di data\n1. Badminton\n2. Tenis meja\n3. Basket\n4. Bola Voli\n5. Exit\nMasukkan angka dari daftar cabor: ");
+                System.out.print("Cabang Olahraga yang akan di data\n1. Badminton\n2. Tenis meja\n3. Basket\n4. Bola Voli\n5. Exit\nMasukkan angka dari daftar CABOR: ");
                 inputCabor = sc.nextLine();
                 if (inputCabor.equalsIgnoreCase("1")){
                     for (int i = 0 ; i < cabor[0].length ; i++){
-                        System.out.print("Masukkan nama atlit ke-" + (i+1) +  " cabor Badminton: ");
+                        System.out.print("Masukkan NAMA ATLIT ke-" + (i+1) +  " CABOR Badminton: ");
                         namaAtlit = sc.nextLine();
                         cabor [0][i] = namaAtlit;
-                        System.out.print("'y' untuk iya 't' untuk tidak\nApakah anda ingin memasukkan nama atlit lagi?: ");
+                        System.out.print("'y' untuk iya 't' untuk tidak\nApakah anda ingin memasukkan NAMA ATLIT lagi?: ");
                         konfirmasi2 = sc.nextLine();
                         if (konfirmasi2.equalsIgnoreCase("t")){
                             break;
@@ -21,10 +21,10 @@
                     }
                 }else if (inputCabor.equalsIgnoreCase("2")){
                     for (int i = 0 ; i < cabor[1].length ; i++){
-                        System.out.print("Masukkan nama atlit ke-" + (i+1) +  " cabor Tenis Meja: ");
+                        System.out.print("Masukkan NAMA ATLIT ke-" + (i+1) +  " CABOR Tenis Meja: ");
                         namaAtlit = sc.nextLine();
                         cabor [1][i] = namaAtlit;
-                        System.out.print("'y' untuk iya 't' untuk tidak\nApakah anda ingin memasukkan nama atlit lagi?: ");
+                        System.out.print("'y' untuk iya 't' untuk tidak\nApakah anda ingin memasukkan NAMA ATLIT lagi?: ");
                         konfirmasi2 = sc.nextLine();
                         if (konfirmasi2.equalsIgnoreCase("t")){
                             break;
@@ -32,10 +32,10 @@
                     }
                 }else if (inputCabor.equalsIgnoreCase("3")){
                     for (int i = 0 ; i < cabor[2].length ; i++){
-                        System.out.print("Masukkan nama atlit ke-" + (i+1) +  " cabor Basket: ");
+                        System.out.print("Masukkan NAMA ATLIT ke-" + (i+1) +  " CABOR Basket: ");
                         namaAtlit = sc.nextLine();
                         cabor [2][i] = namaAtlit;
-                        System.out.print("'y' untuk iya 't' untuk tidak\nApakah anda ingin memasukkan nama atlit lagi?: ");
+                        System.out.print("'y' untuk iya 't' untuk tidak\nApakah anda ingin memasukkan NAMA ATLIT lagi?: ");
                         konfirmasi2 = sc.nextLine();
                         if (konfirmasi2.equalsIgnoreCase("t")){
                             break;
@@ -43,17 +43,17 @@
                     }
                 }else if (inputCabor.equalsIgnoreCase("4")){
                     for (int i = 0 ; i < cabor[3].length ; i++){
-                        System.out.print("Masukkan nama atlit ke-" + (i+1) +  " cabor Bola Voli: ");
+                        System.out.print("Masukkan NAMA ATLIT ke-" + (i+1) +  " CABOR Bola Voli: ");
                         namaAtlit = sc.nextLine();
                         cabor [3][i] = namaAtlit;
-                        System.out.print("'y' untuk iya 't' untuk tidak\nApakah anda ingin memasukkan nama atlit lagi?: ");
+                        System.out.print("'y' untuk iya 't' untuk tidak\nApakah anda ingin memasukkan NAMA ATLIT lagi?: ");
                         konfirmasi2 = sc.nextLine();
                         if (konfirmasi2.equalsIgnoreCase("t")){
                             break;
                         }
                     }
                 }
-                System.out.print("'y' untuk iya dan 't' untuk tidak\nApakah anda ingin input atlit dari cabor lain?: ");
+                System.out.print("'y' untuk iya dan 't' untuk tidak\nApakah anda ingin input NAMA ATLIT dari CABOR lain?: ");
                 konfirmasi = sc.nextLine();
             }while (konfirmasi.equalsIgnoreCase("y"));
             sc.close();
